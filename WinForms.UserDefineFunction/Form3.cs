bce514347715148177040a8048a520496b70a4da
thegig009 <loveesesgig2005@gmail.com>
2025-12-24 11:15:20 +0700

    Update Form3.cs

--- WinForms.UserDefineFunction/Form3.cs
+++ WinForms.UserDefineFunction/Form3.cs
@@ -1,42 +1,73 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
+using System;
 using System.Windows.Forms;
 
 namespace WinForms.UserDefineFunction
 {
-    public partial class Form3 : Form
-    {
-        public Form3()
-        {
-            InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            string letter = textBox1.Text;
-            int size = int.Parse(textBox2.Text);
-           
-            if (radioButton1.Checked)
-            {
-                
-				textBox3.Text = letterTriangle1(letter, size);
-			}
-            else if (radioButton2.Checked)
-            {
-           
-				textBox3.Text = letterTriangle2(letter);
-			}
-            else if (radioButton3.Checked)
-            {
-                letterTriangle3(letter, size, out string result);
-			}
-            
+	public partial class Form3 : Form
+	{
+		public Form3()
+		{
+			InitializeComponent();
 		}
 
+		private void button1_Click(object sender, EventArgs e)
+		{
+			string letter = textBox1.Text;
+			int size = int.Parse(textBox2.Text);
 
-    }
+			if (radioButton1.Checked)
+			{
+				textBox3.Text = Triangle1(letter, size);
+			}
+			else if (radioButton2.Checked)
+			{
+				textBox3.Text = Triangle2(letter);
+			}
+			else if (radioButton3.Checked)
+			{
+				
+				string result;
+				Triangle3(letter, size, out result);
+				textBox3.Text = result;
+			}
+		}
+
+		private string Triangle1(string letter, int size)
+		{
+			string result = "";
+			char ch = letter[0];
+
+			for (int i = 1; i <= size; i++)
+			{
+				result += new string(ch, i) + Environment.NewLine;
+			}
+			return result;
+		}
+
+	
+		private string Triangle2(string letter, int size = 5)
+		{
+			string result = "";
+			char ch = letter[0];
+
+			for (int i = 1; i <= size; i++)
+			{
+				result += new string(ch, i) + Environment.NewLine;
+			}
+			return result;
+		}
+
+		
+		private void Triangle3(string letter, int size, out string result)
+		{
+			result = "";
+			char ch = letter[0];
+
+			for (int i = 1; i <= size; i++)
+			{
+				result += new string(ch, i) + Environment.NewLine;
+			}
+		}
+	}
 }
+
